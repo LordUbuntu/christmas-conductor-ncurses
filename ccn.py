a3b4171ecb10d805e740bad3ff7df3325149c541
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-03 23:52:32 -0800

    add: feature ideas

--- ccn.py
+++ ccn.py
@@ -1,6 +1,9 @@
 # Jacobus Burger (2024-12-16)
 # Power outage happened recently for a second before I assume the backup generator of the grid kicked in. This inspired a simple ascii game idea where you type out the letter of a station when it goes out to power it back on, can't leave a station off for too long or you lose! This may be a full project so for now here's a little placeholder:
 # TODO: write feature list and quickly implement
+# - ncurses window based graphics for better visuals
+# - keyboard letter based "switches" that redirect power
+# - procedurally generated levels
 import termcolor
 
 def placeholder():
